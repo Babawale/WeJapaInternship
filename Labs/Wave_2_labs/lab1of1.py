706fc9a8a4a9d787ca72e66159318029d4682be7
Olatunde Babawale <savviscribe@gmail.com>
2020-08-04 12:33:33 +0000

    fixed month variable

--- Labs/Wave_2_labs/lab1of1.py
+++ Labs/Wave_2_labs/lab1of1.py
@@ -9,6 +9,6 @@ month = 8
 days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
 
 # use list indexing to determine the number of days in month
-num_days = days_in_month[7]
+num_days = days_in_month[month - 1]
 
 print(num_days)
