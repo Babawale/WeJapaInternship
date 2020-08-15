1c226c619a36223c19cde5842901828362ed456a
Olatunde Babawale <savviscribe@gmail.com>
2020-08-15 22:29:26 +0000

    completed match_flower_name.py

--- Labs/Wave_4_labs/Scripting labs/match_flower_name.py
+++ Labs/Wave_4_labs/Scripting labs/match_flower_name.py
@@ -10,6 +10,32 @@
 # Write your code here
 
 # HINT: create a dictionary from flowers.txt
+def flowers_dict(filename):
+    """
+    This function opens the flowers.txt, reads every line in it, and saves it as a dictionary.
+    
+    """
+    flowers = {} #initialize the flowers' dictionary
+    with open(filename) as f: # open the text file as f.
+        for line in f: # reads every line in text file.
+            letter = line.split(": ")[0].lower() # splits the line by ":" and takes the first index i.e letter in lower case
+            flower = line.split(": ")[1] # splits the line by ":" and takes the second index i.e flower
+            #print(flower)
+            flowers[letter] = flower # enters the letter and flower as value pairs in the flowers' dictionary.
+    return flowers
 
 # HINT: create a function
-        
+    
+def main():
+    """
+    The main function as described in the instructions.
+    
+    """
+    flowers = flowers_dict('flowers.txt') # creates the dictionary
+    full_name = input("Enter your First [space] Last name only: ") # user input
+    first_name = full_name[0].lower() # takes the first index of the full_name i.e first name.
+    first_letter = first_name[0] # takes the first letter (index) of the first name.
+
+    print("Unique flower name with the first letter: {}".format(flowers[first_letter])) # prints the flower name that matches the first letter
+
+main() # call the main function
